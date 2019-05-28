6d28d8d1f3e6da8a84f81011e67e6ca363c389fc
Jason Lindsey <jasonlindsey@Jasons-MacBook-Pro.local>
2019-05-28 15:04:00 -0500

    search add show

---
+++ src/ShowContacts.java
@@ -0,0 +1,93 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ShowContacts {
+    public static void showContacts(String directory, String filename){
+        Path file = Paths.get(directory, filename);
+        try {
+            List<String> contactList = Files.readAllLines(file);
+            System.out.println("Name | Phone number\n" +
+                    "-----------------");
+            for(String contact: contactList){
+                String name = contact.split(" - ")[0];
+                String number = contact.split(" - ")[1];
+                System.out.println(name + " | " + number);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addContact(String directory, String filename, ArrayList<String> list){
+        try {
+            Files.write(
+                    Paths.get(directory, filename),
+                    list,
+                    StandardOpenOption.APPEND
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String searchContact(String directory, String filename, String contact){
+        Path file = Paths.get(directory, filename);
+            String output = "Contact not found";
+        try {
+            List<String> contactList = Files.readAllLines(file);
+            for(String item: contactList){
+                if(item.contains(contact)){
+                    String name = item.split("-")[0];
+                    String number = item.split("-")[1];
+                    return output = name + " | " + number;
+                }
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        } return output;
+    }
+
+    public static void main(String[] args) {
+        String directory = "data";
+        String filename = "contacts.txt";
+
+        Path dataDirectory = Paths.get(directory);
+        Path dataFile = Paths.get(directory, filename);
+        if (Files.notExists(dataFile)) {
+            try {
+                Files.createDirectories(dataDirectory);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        if (! Files.exists(dataFile)) {
+
+            try {
+                Files.createFile(dataFile);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        ArrayList<String> contacts = new ArrayList<>(List.of("Nadia - 1234567890", "jason - 0987654321"));
+
+
+        showContacts(directory, filename);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("who do you want to search?");
+        String search = sc.nextLine();
+
+        System.out.println(searchContact(directory, filename, search));
+
+    }
+}
